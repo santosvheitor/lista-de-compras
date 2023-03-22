d42acc725cb4dda5af3abefd220cf7e501f9134f
santosvheitor <santosvheitor@gmail.com>
2023-03-22 17:44:48 +0000

    lista-de-compras

---
+++ desafios.js
@@ -0,0 +1,9 @@
+cliente = {
+    nome: 'Mariana', 
+    idade: 35, 
+    genero: "Feminino"}
+ 
+
+cliente.estado = 'Pernambuco';
+
+console.log('Olá, meu nome é ' + cliente.nome + ' e moro no estado de ' + cliente.estado + '.');
